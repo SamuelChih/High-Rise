75d440e9fb40afc58d1d22a409964241ea7496a8
Selim <selim_frljuckic@yahoo.com>
2022-11-04 01:13:43 -0700

    Updated PlayerMovement Class with WallJump Mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpspeed;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
 
     private Rigidbody2D body;
     private Animator anim;
     private BoxCollider2D boxCollider;
-    
+    private float wallJumpCooldown;
+    private float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     private void Awake()
     {
@@ -23,36 +31,59 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
 
-        body.velocity = new Vector2(horizontalInput * speed,body.velocity.y); // Gets x axis input from player
+        //Flip player when moving left-right
 
-
-    
-        if(horizontalInput < -0.01f) // If player is moving left, face left
-        {
+        if (horizontalInput < -0.01f) // If player is moving left, face left
             transform.localScale = Vector3.one;
-        }
-        else if(horizontalInput > 0.01f) // If player is facing right, face right
-        {
+        else if (horizontalInput > 0.01f) // If player is moving right, face right
             transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-
-
-        if(Input.GetKey(KeyCode.Space) && isGrounded()) // Gets jump input from player
-        {
-            Jump();
-        }
 
         //Set animator parameters
         anim.SetBool("run", horizontalInput != 0);
+        // anim.SetBool("grounded", isGrounded());
 
+        //Wall jump logic
+        if (wallJumpCooldown > 0.2f)
+        {
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else
+                body.gravityScale = 7;
+
+            if (Input.GetKey(KeyCode.Space))
+                Jump();
+        }
+        else
+            wallJumpCooldown += Time.deltaTime;
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, jumpspeed);
+        if(isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpspeed);
+            // anim.SetTrigger("jump");
+        }
+
+        else if(onWall() && !isGrounded())
+        {
+            if(horizontalInput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 4, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else 
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 2, 4);
+
+            wallJumpCooldown = 0;
+        }
     }
 
     private bool isGrounded()
@@ -61,5 +92,10 @@ public class PlayerMovement : MonoBehaviour
         return raycastHit.collider != null;
     }
 
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
     
 }
